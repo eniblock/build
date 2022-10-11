40b33501fbed0368cbae816620a008be13c91f7c
Samuel Loury <konubinixweb@gmail.com>
2022-10-11 15:14:36 +0200

    ci: Create an action that may be used to find out the build tag to use in our projects

---
+++ actions/buildtag/script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+COMMIT_REF_SLUG="${GITHUB_REF_NAME}"
+PIPELINE_ID="${GITHUB_RUN_NUMBER}"
+if echo "${GITHUB_REF}"|grep -q 'refs/tags/'
+then
+    COMMIT_TAG="${GITHUB_REF_NAME}"
+else
+    COMMIT_TAG=""
+fi
+
+
+helmdir=$1
+
+if [ -z "$helmdir" ]; then
+  if [ -z "$COMMIT_TAG" ]; then
+    echo $COMMIT_REF_SLUG.$PIPELINE_ID
+  else
+    echo $COMMIT_TAG
+  fi
+else
+  version=`cat $helmdir/Chart.yaml|grep '^version:'|tr -d '"' |sed -r 's/^version: *(.+)$/\1/'`
+  if [ -z "${version}" ]; then
+    cat <<EOF >> /dev/stderr
+Could not compute the version of the helm chart in ${helmdir}/Chart.yaml.
+Please make sure it contains a line starting with 'version: some.version.number1'
+EOF
+    exit 1
+  fi
+  if [ -z "$COMMIT_TAG" ]; then
+    version=$version-$COMMIT_REF_SLUG
+    echo $version.$PIPELINE_ID
+  else
+    if [ "$version" != "$COMMIT_TAG" ]; then
+      echo Chart version and tag mismatch: "$version" vs "$COMMIT_TAG" >> /dev/stderr
+      exit 1
+    fi
+    echo $version
+  fi
+fi
